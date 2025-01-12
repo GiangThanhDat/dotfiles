7f4123e53c6407e2c7c7e1149048bf1d00e70be6
Giang Thanh Dat <gthanhdatpro@gmail.com>
2025-01-12 12:43:26 +0700

    fix(create symlinks): create symlinks doesn't need specific destination path, only path to parent folder(~/.config)

--- install.sh
+++ install.sh
@@ -1,27 +1,26 @@
-
-#!/bin/bash 
+#!/bin/bash
 
 # Install prerequisites
 echo "Installing prerequisites..."
-if [[ "$OSTYPE" == 'linux-gnu'*]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt update
   sudo apt install -y fish tmux git neovim curl
-elif [[ "$OSTYPE" == 'darwin'*]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew update
   brew install fish tmux git neovim
-fi 
+fi
 
-# Create symlinks 
+# Create symlinks
 echo "Creating symlinks"
-ln -sf ~/dotfiles/.config/fish ~/.config/fish 
-ln -sf ~/dotfiles/.config/nvim ~/.config/nvim 
-ln -sf ~/dotfiles/.config/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.config/fish ~/.config
+ln -sf ~/dotfiles/.config/nvim ~/.config
+ln -sf ~/dotfiles/.config/tmux/.tmux.conf ~/
 
-# Set default shell to fish 
-if ! grep -q "$(which fish)" /etc/shells; then 
+# Set default shell to fish
+if ! grep -q "$(which fish)" /etc/shells; then
   echo "Adding fish to valid shells..."
-  echo "$(which fish)" | sudo tee -a /tec/shells 
-fi 
+  echo "$(which fish)" | sudo tee -a /tec/shells
+fi
 
 chsh -s $(which fish)
 
